6e3bc857e6d535c9f4fa28db8b800c896a11694a
MissSi <lucky.yaoxiaojuan@gmail.com>
2018-03-07 17:20:07 +0800

    reorder class names

--- src/js/app.js
+++ src/js/app.js
@@ -8,11 +8,11 @@ const getChildDom = (childData) => {
   if (childData.length === 0) {
     return '';
   }
-  let str = '<ul class="tree-child-tree">';
+  let str = '<ul class="tree__child__tree">';
   childData.map((node) => {
     const childLength = node.children.length;
-    const nodeClasses = childLength === 0 ? 'tree-node' : 'tree-switcher tree-switcher_close tree-node';
-    str += '<li>';
+    const nodeClasses = childLength === 0 ? 'tree__node' : 'tree__switcher tree__switcher--close tree__node';
+    str += '<li class="tree__branch">';
     str += getNodeDom(node.data, nodeClasses);
     if (childLength === 0) {
       str += '</li>';
@@ -58,11 +58,11 @@ const getTreeData = () => {
 window.addEventListener('load', () => {
   // 绑定节点toggle事件
   const bindNodeToggleEvent = () => {
-    const treeSwitcher = document.querySelectorAll('.tree-switcher');
+    const treeSwitcher = document.querySelectorAll('.tree__switcher');
     treeSwitcher.forEach((switcher) => {
       switcher.addEventListener('click', () => {
-        switcher.classList.toggle('tree-switcher_close');
-        switcher.classList.toggle('tree-switcher_open');
+        switcher.classList.toggle('tree__switcher--close');
+        switcher.classList.toggle('tree__switcher--open');
         switcher.nextElementSibling.classList.toggle('hidden');
       });
     });
@@ -70,7 +70,7 @@ window.addEventListener('load', () => {
   // 重绘DOM树
   const setTreeContent = (treeData) => {
     const treeDom = document.getElementById('tree');
-    treeDom.innerHTML = getNodeDom(treeData.root.data, 'tree-switcher tree-switcher_close tree-node') + getChildDom(treeData.root.children);
+    treeDom.innerHTML = getNodeDom(treeData.root.data, 'tree__switcher tree__switcher--close tree__node') + getChildDom(treeData.root.children);
     bindNodeToggleEvent();
   };
   // 绑定random data事件
@@ -84,10 +84,10 @@ window.addEventListener('load', () => {
   const bindOpenAllEvent = () => {
     const openAllBtn = document.getElementById('open');
     openAllBtn.addEventListener('click', () => {
-      const treeSwitcher = document.querySelectorAll('.tree-switcher');
+      const treeSwitcher = document.querySelectorAll('.tree__switcher');
       treeSwitcher.forEach((switcher) => {
-        switcher.classList.remove('tree-switcher_open');
-        switcher.classList.add('tree-switcher_close');
+        switcher.classList.remove('tree__switcher--open');
+        switcher.classList.add('tree__switcher--close');
         switcher.nextElementSibling.classList.remove('hidden');
       });
     });
@@ -96,10 +96,10 @@ window.addEventListener('load', () => {
   const bindCloseAllEvent = () => {
     const openAllBtn = document.getElementById('close');
     openAllBtn.addEventListener('click', () => {
-      const treeSwitcher = document.querySelectorAll('.tree-switcher');
+      const treeSwitcher = document.querySelectorAll('.tree__switcher');
       treeSwitcher.forEach((switcher) => {
-        switcher.classList.add('tree-switcher_open');
-        switcher.classList.remove('tree-switcher_close');
+        switcher.classList.add('tree__switcher--open');
+        switcher.classList.remove('tree__switcher--close');
         switcher.nextElementSibling.classList.add('hidden');
       });
     });
