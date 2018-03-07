1564dc4092026c1bbabdf214cd2b27fe9ea9151b
MissSi <lucky.yaoxiaojuan@gmail.com>
2018-03-07 10:40:31 +0800

    random tree data done

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
-// 通用方法
-const randomNum = max => Math.ceil(Math.random() * max);
-const randomStr = () => Math.random().toString(36).substr(2);
+// 通用
+const getRandomNum = max => Math.ceil(Math.random() * max);
+const getRandomStr = () => Math.random().toString(36).substr(2);
 
-// 获取节点模板的方法
+// 获取节点模板
 const getNodeDom = node => `<span class="tree-switcher tree-switcher_close tree-node">${node.text}</span>`;
 const getChildDom = (childData) => {
   if (childData.length === 0) {
@@ -24,49 +24,48 @@ const getChildDom = (childData) => {
 };
 
 // 随机生成树结构数据
-// TODO: 优化算法
-const randomData = (num, total) => {
-  const nodes = [];
-  if (num === 0 || total === 0) {
+const getTreeData = () => {
+  // length -> 兄弟节点个数
+  // deep -> 树深度
+  const randomTreeNodes = (length, deep) => {
+    const nodes = [];
+    for (let i = 0; i < length && deep > 0; i += 1) {
+      const data = {
+        data: {
+          id: getRandomStr(),
+          text: getRandomStr(),
+        },
+        children: [],
+      };
+      if (deep > 0) data.children = randomTreeNodes(getRandomNum(3), deep - 1);
+      nodes.push(data);
+    }
     return nodes;
-  }
-  for (let i = 0; i < num; i += 1) {
-    const data = {
-      data: {
-        id: randomStr(),
-        text: randomStr(),
-      },
-      children: [],
-    };
-    data.children = randomData(randomNum(5), total - 1);
-    nodes.push(data);
-  }
-  return nodes;
-};
-
-// 设置树结构内容
-const setTreeContent = () => {
-  const tree = document.getElementById('tree');
-  const data = {
+  };
+  return ({
     root: {
       data: {
-        id: 'd9f0899f4fdf',
+        id: getRandomStr(),
         text: '根节点',
       },
-      children: randomData(randomNum(2), 3),
+      children: randomTreeNodes(getRandomNum(2), getRandomNum(10)),
     },
-  };
-  tree.innerHTML = getNodeDom(data.root.data) + getChildDom(data.root.children);
+  });
 };
 
-setTreeContent();
-
-// TODO: 优化random事件绑定方法
-const randomBtn = document.getElementById('random');
-randomBtn.addEventListener('click', () => {
-  setTreeContent();
+window.addEventListener('load', () => {
+  // 重绘DOM树
+  const setTreeContent = (treeData) => {
+    const treeDom = document.getElementById('tree');
+    treeDom.innerHTML = getNodeDom(treeData.root.data) + getChildDom(treeData.root.children);
+  };
+  setTreeContent(getTreeData());
+  // 绑定random data事件
+  const randomBtnDom = document.getElementById('random');
+  randomBtnDom.addEventListener('click', () => {
+    setTreeContent(getTreeData());
+  });
 });
-
 
 // 通过Json文件获取数据
 // const getJSON = (url, callback) => {
